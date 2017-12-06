abcb26a41d45dcf3c7d62f6e9a211649161c19ce
Sebastian Arce <Sebas@Sebastians-MacBook-Pro-3.local>
2017-12-05 19:45:41 -0600

    first commit

---
+++ server/controllers/tasks.js
@@ -0,0 +1,22 @@
+var basicAuth = require('basic-auth');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var modelos = require ('../models/modelos').modelos;
+var express = require('express');
+var app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+
+exports.guardar = function(req, res){ 
+
+	res.status(400).send('Something broke!');
+
+}
+exports.regresar = function(req, res){ 
+
+}
+exports.eliminar = function(req, res, id){ 
+
+}
+exports.token = function(req, res){ 
+
+}
--- server/router.js
+++ server/router.js
@@ -3,22 +3,22 @@ const multer = require('multer');
 const upload = multer();
 const jsonParser = bodyParser.json();
 const urlencoderParser = bodyParser.urlencoded({extended: true});
-
+const cookieParser = require('cookie-parser');
 
 var express = require('express')
 var app = express()
 
 const ControllerTask = require('./controllers/tasks');
 
+app.use(bodyParser.json());
+
 // respond with "hello world" when a GET request is made to the homepage
 
-app.post('/api/tasks', function (req, res) {
-  res.send('POST request to the homepage')
-})
-app.get('/api/tasks', function (req, res) {
-  res.send('GET request to the homepage')
-})
-app.post('/api/auth/token', function (req, res) {
-  res.send('POST request to the homepage')
-})
+app.post('/api/tasks', tasks.guardar);
+
+app.get('/api/tasks', tasks.regresar);
+
+app.delete('/api/tasks/:id', tasks.eliminar);
+
+app.post('/api/auth/token', tasks.token );
 exports.app = app
